6f32c52734204f9485affca148f4ed24c2408fd1
ayush578 <ayush.agrawal.cse20@itbhu.ac.in>
2022-10-26 14:42:58 +0530

    ayush4

---
+++ DBMSProject/src/main/java/com/AADHA/Starters/DBMSProject/model/works_in.java
@@ -0,0 +1,38 @@
+package com.AADHA.Starters.DBMSProject.model;
+
+public class works_in {
+    int emp_id;
+    String dept_name,joining_date,leaving_date;
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getDept_name() {
+        return dept_name;
+    }
+
+    public void setDept_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public String getJoining_date() {
+        return joining_date;
+    }
+
+    public void setJoining_date(String joining_date) {
+        this.joining_date = joining_date;
+    }
+
+    public String getLeaving_date() {
+        return leaving_date;
+    }
+
+    public void setLeaving_date(String leaving_date) {
+        this.leaving_date = leaving_date;
+    }
+}
